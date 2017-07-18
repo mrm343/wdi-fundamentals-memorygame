4d657e51a96e109640453676175d5c9582f4301a
MOREYRA <mrm343@cornell.edu>
2017-07-18 15:19:53 -0400

    unit 9 final

--- js/main.js
+++ js/main.js
@@ -2,18 +2,20 @@ console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
 
-if(cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]){
-    alert("You found a match!");
+function checkForMatch() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
+  } else {
+    console.log("Sorry, try again.");
   }
-  else {
-    alert("Sorry, try again.");
+}
+
+function flipCard(cardId){
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+
+  if(cardsInPlay.length === 2) {
+    checkForMatch();
   }
 }
